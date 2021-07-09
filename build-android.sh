0b25e0791e2fa43b14c597aa6dc57750b8b0b3a4
MIHOYO\zhao.li <rioleehit@foxmail.com>
2021-07-09 14:29:50 +0800

    -

--- build-android.sh
+++ build-android.sh
@@ -8,12 +8,16 @@ SCRIPTPATH=`pwd`
 # SCRIPTPATH=`dirname $0`
 echo $SCRIPTPATH
 OPENSSL_DIR=$SCRIPTPATH/submodules/openssl
+
+# Create the make file
+cd ${OPENSSL_DIR}
+
 OPENSSL_PREFIX=$SCRIPTPATH/build/
 export ANDROID_NDK_HOME=$SCRIPTPATH/android-ndk-r21e
 export ANDROID_NDK_ROOT=$SCRIPTPATH/android-ndk-r21e
 
 # Find the toolchain for your build machine
-toolchains_path=$(python3 toolchains_path.py --ndk ${ANDROID_NDK_HOME})
+toolchains_path=$(python3 ../../toolchains_path.py --ndk ${ANDROID_NDK_HOME})
 
 # Configure the OpenSSL environment, refer to NOTES.ANDROID in OPENSSL_DIR
 # Set compiler clang, instead of gcc by default
@@ -29,12 +33,11 @@ ANDROID_API=21
 # Can be android-arm, android-arm64, android-x86, android-x86 etc
 architecture=android-arm
 
-# Create the make file
-cd ${OPENSSL_DIR}
-./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API --prefix=$OPENSSL_PREFIX
-# ./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API -DOPENSSL_NO_STDIO enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp no-autoerrinit no-filenames no-ui-console no-err no-zlib no-egd no-idea no-rc5 no-rc4 no-afalgeng no-comp no-cms no-ct no-srp no-srtp no-ts no-gost no-dso no-ec2m no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-des no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt no-weak-ssl-ciphers no-shared no-tests -DL_ENDIAN 
-# ./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API -DOPENSSL_NO_STDIO enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp no-autoerrinit no-filenames no-ui-console no-err no-zlib no-egd no-uplink no-idea no-rc5 no-rc4 no-afalgeng no-acvp_tests no-comp no-cmp no-cms no-ct no-srp no-srtp no-ts no-fips no-gost no-padlockeng no-dso no-ec2m no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method no-siv no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-des no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt no-weak-ssl-ciphers no-shared no-tests -DL_ENDIAN 
+# ./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API --prefix=$OPENSSL_PREFIX enable-tls1_3
+# ./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API -DOPENSSL_NO_STDIO enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp no-autoerrinit no-filenames no-ui-console no-err no-zlib no-egd no-uplink no-idea no-rc5 no-rc4 no-afalgeng no-acvp_tests no-comp no-cmp no-cms no-ct no-srp no-srtp no-ts no-fips no-gost no-padlockeng no-dso no-ec2m no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method no-siv no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-des no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt no-weak-ssl-ciphers no-shared no-tests -DL_ENDIAN --prefix=$(pwd)/android-arm
+./Configure ${architecture} -D__ANDROID_API__=$ANDROID_API -DOPENSSL_NO_STDIO enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp no-autoerrinit no-filenames no-ui-console no-err no-zlib no-egd no-idea no-rc5 no-rc4 no-afalgeng no-comp no-cms no-ct no-srp no-srtp no-ts no-gost no-dso no-ec2m no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-des no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt no-weak-ssl-ciphers no-shared no-tests -DL_ENDIAN
 # --prefix=$OPENSSL_PREFIX
+#  --prefix=$(pwd)/android-arm
 # -DOPENSSL_NO_STDIO enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp no-autoerrinit no-filenames no-ui-console no-err no-zlib no-egd no-idea no-rc5 no-rc4 no-afalgeng no-comp no-cms no-ct no-srp no-srtp no-ts no-gost no-dso no-ec2m no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-des no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt no-weak-ssl-ciphers no-shared no-tests -DL_ENDIAN 
 # Build
 make
